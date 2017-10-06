d0b861fe884bb02c215dd648e766bbac26b27219
Vasili Vasilyeu <vasili.v@tut.by>
2017-10-06 17:16:52 +0300

    Added deployment scripts

---
+++ ust-client/aws/deploy-client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+sudo tar -xzf /home/ec2-user/go1.8.3.linux-amd64.tar.gz -C /usr/local/
+sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
+go version
+
+sudo yum install -y git
+mkdir -p src/github.com/vasili-v
+cd src/github.com/vasili-v/
+git clone https://github.com/vasili-v/udp-stream-test
+cd udp-stream-test/ust-client
+
+export GOPATH=$HOME
+export PATH=$GOPATH/bin:$PATH
+go get -u
+go install
